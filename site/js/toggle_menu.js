dece6de0d163d8ac0abb18e06e636627c2b31905
itggot-edvin-ketabati-augustinsson <edvin.ketabatiaugustinsson@itggot.se>
2018-05-22 11:12:19 +0200

    Almost finished login page, styled input forms and created a button

--- site/js/toggle_menu.js
+++ site/js/toggle_menu.js
@@ -1,7 +1,4 @@
 function toggleMenu(){
     var element = document.querySelector(".menu_div");
     element.classList.toggle("show_menu_div");
-    
-    element = document.querySelector("main");
-    element.classList.toggle("fade")
 }
